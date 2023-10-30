d630509976a145969a9b963c0a916bea2ac20655
kapsesrikanth <112060563+kapsesrikanth@users.noreply.github.com>
2023-10-30 12:07:34 +0530

    Third Commit

--- DemoPush/src/pack1/Welcome.java
+++ DemoPush/src/pack1/Welcome.java
@@ -6,6 +6,7 @@ public class Welcome {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to GPREC");
 		System.out.println("Welcome to CSE");
+		System.out.println("Welcome to ECS");
 	}
 
 }
